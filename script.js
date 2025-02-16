2aec8ac799189d8f0d6fb0fd6fe00c7d7af3e2c3
Noureddine <121136079+NoureddineBou@users.noreply.github.com>
2025-02-16 16:14:15 +0100

    adding emoji picker

--- script.js
+++ script.js
@@ -148,6 +148,30 @@ fileInput.addEventListener("change", () => {
   reader.readAsDataURL(file);
 });
 
+// Emoji picker setup
+const picker = new EmojiMart.Picker({
+  theme: "light",
+  skinTonePosition: "none",
+  preview: "none",
+  onEmojiSelect: (emoji) => {
+    const { selectionStart: start, selectionEnd: end } = messageInput;
+    messageInput.setRangeText(emoji.native, start, end, "end");
+    messageInput.focus();
+  },
+  onClickOutside: (e) => {
+    if (e.target.id === "emoji-picker") {
+      document.body.classList.toggle("show-emoji-picker");
+    } else {
+      document.body.classList.remove("show-emoji-picker");
+    }
+  }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
+
+
+
+
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
 document
   .querySelector("#file-upload")
