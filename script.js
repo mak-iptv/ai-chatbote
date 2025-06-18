0fcac61803e347df29aa335fd08b03f7f81c8561
Bouderbala Noureddine <121136079+nxr-deen@users.noreply.github.com>
2025-06-18 11:35:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyChSWxGCh2WzEqHFDawqiW0SIbuD_PBepo";
+const API_KEY = "#####";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
