5819c8761eabfee0baf0596204fa66fbcea4023f
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-10-24 22:58:24 +0300

    Update API_URL with new API_KEY format

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyC_o6YdAYl6eT9waZqieWboFMqoGLKVsms";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyC_o6YdAYl6eT9waZqieWboFMqoGLKVsms`;
 
 const userData = {
   message: null,
